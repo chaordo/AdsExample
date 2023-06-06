9a5ddfdf548e7fb306fc032fdd8585410bb7701e
quintus <john@chaordo.com>
2023-06-06 16:08:26 -0600

    Initial Commit

---
+++ AdsExample/AdsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdsExampleApp.swift
+//  AdsExample
+//
+//  Created by John on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct AdsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
